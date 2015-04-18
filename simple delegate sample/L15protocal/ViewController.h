b1ad4d01a059f4384d74b34f442a89d07342e0be
light_bo <785250288@qq.com>
2015-04-18 09:30:34 +0800

    Delegate
    A simple sample anout delegate!

---
+++ simple delegate sample/L15protocal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  L15protocal
+//
+//  Created by plter on 14-4-22.
+//  Copyright (c) 2014年 eoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ simple delegate sample/L15protocalTests/L15protocalTests.m
@@ -0,0 +1,34 @@
+//
+//  L15protocalTests.m
+//  L15protocalTests
+//
+//  Created by plter on 14-4-22.
+//  Copyright (c) 2014年 eoe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface L15protocalTests : XCTestCase
+
+@end
+
+@implementation L15protocalTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
