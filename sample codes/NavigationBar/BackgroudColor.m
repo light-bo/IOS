536d65189a9089645ac3d3b9c5016e6d5fc3a7f9
gzyiniu <gzyiniu@gzyiniudeMac-mini-2.local>
2015-05-23 15:42:28 +0800

    navigation
    add the code to change the navigation control background color.

--- sample codes/NavigationBar/BackgroudColor.m
+++ sample codes/NavigationBar/BackgroudColor.m
@@ -1,3 +1,11 @@
-//设置导航栏的背景颜色
-[[UINavigationBar appearance] setBarTintColor:Theme_Color];
+/*************************************************************************/
+//ios7以下的版本设置导航栏背景颜色可以使用
+[[UINavigationBar appearance] setTintColor:[UIColor orangeColor]];
 
+//ios7以后：
+[[UINavigationBar appearance] setBarTintColor:[UIColor orangeColor]];
+
+//默认带有一定透明效果，可以使用以下方法去除系统效果
+[navigationController.navigationBar setTranslucent:NO];
+
+/*************************************************************************/
