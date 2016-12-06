ed86d51a8896cae4955d2c153e05a7ccf8049df7
light_bo <light_bo@126.com>
2016-12-06 11:08:44 +0800

    add BLLNotification.m file

---
+++ OC/sample codes/Notification/BLLNotification.m
@@ -0,0 +1,135 @@
+///=============================================================================
+/// @name 初始化UNUserNotificationCenter
+///=============================================================================
+
+/**
+ * 初始化UNUserNotificationCenter
+ */
+- (void)registerForRemoteNotification {
+    // iOS 10 兼容
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.0")) {
+        
+#if XCODE_VERSION_GREATER_THAN_OR_EQUAL_TO_8
+        
+        // 使用 UNUserNotificationCenter 来管理通知
+        UNUserNotificationCenter *uncenter = [UNUserNotificationCenter currentNotificationCenter];
+        // 监听回调事件
+        [uncenter setDelegate:self];
+        //iOS 10 使用以下方法注册，才能得到授权
+        [uncenter requestAuthorizationWithOptions:(UNAuthorizationOptionAlert+UNAuthorizationOptionBadge+UNAuthorizationOptionSound)
+                                completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                                    [[UIApplication sharedApplication] registerForRemoteNotifications];
+                                    //TODO:授权状态改变
+                                    NSLog(@"%@" , granted ? @"授权成功" : @"授权失败");
+                                }];
+        // 获取当前的通知授权状态, UNNotificationSettings
+        [uncenter getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+            NSLog(@"%s\nline:%@\n-----\n%@\n\n", __func__, @(__LINE__), settings);
+            /*
+             UNAuthorizationStatusNotDetermined : 没有做出选择
+             UNAuthorizationStatusDenied : 用户未授权
+             UNAuthorizationStatusAuthorized ：用户已授权
+             */
+            if (settings.authorizationStatus == UNAuthorizationStatusNotDetermined) {
+                NSLog(@"未选择");
+            } else if (settings.authorizationStatus == UNAuthorizationStatusDenied) {
+                NSLog(@"未授权");
+            } else if (settings.authorizationStatus == UNAuthorizationStatusAuthorized) {
+                NSLog(@"已授权");
+            }
+        }];
+        
+#endif
+        
+    }
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    else if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0")) {
+        UIUserNotificationType types = UIUserNotificationTypeAlert |
+                                       UIUserNotificationTypeBadge |
+                                       UIUserNotificationTypeSound;
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+    } else {
+        UIRemoteNotificationType types = UIRemoteNotificationTypeBadge |
+                                         UIRemoteNotificationTypeAlert |
+                                         UIRemoteNotificationTypeSound;
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:types];
+    }
+#pragma clang diagnostic pop
+}
+
+#pragma mark UNUserNotificationCenterDelegate
+
+#pragma mark - 添加处理 APNs 通知回调方法
+///=============================================================================
+/// @name 添加处理APNs通知回调方法
+///=============================================================================
+
+#pragma mark -
+#pragma mark - UNUserNotificationCenterDelegate Method
+
+#if XCODE_VERSION_GREATER_THAN_OR_EQUAL_TO_8
+
+/**
+ * Required for iOS 10+
+ * 在前台收到推送内容, 执行的方法
+ */
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+    NSDictionary *userInfo = notification.request.content.userInfo;
+    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        //TODO:处理远程推送内容
+        NSLog(@"%@", userInfo);
+    }
+    // 需要执行这个方法，选择是否提醒用户，有 Badge、Sound、Alert 三种类型可以选择设置
+    completionHandler(UNNotificationPresentationOptionAlert);
+}
+
+/**
+ * Required for iOS 10+
+ * 在后台和启动之前收到推送内容, 点击推送后执行的方法
+ */
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)())completionHandler {
+    NSDictionary *userInfo = response.notification.request.content.userInfo;
+    if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        //TODO:处理远程推送内容
+        NSLog(@"%@", userInfo);
+    }
+    completionHandler();
+}
+
+#endif
+
+#pragma mark -
+#pragma mark - UIApplicationDelegate Method
+
+/*!
+ * Required for iOS 7+
+ */
+- (void)application:(UIApplication *)application
+didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    //TODO:处理远程推送内容
+    NSLog(@"%@", userInfo);
+    // Must be called when finished
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+#pragma mark - 实现注册APNs失败接口（可选）
+///=============================================================================
+/// @name 实现注册APNs失败接口（可选）
+///=============================================================================
+
+/**
+ * also used in iOS10
+ */
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"%s\n[无法注册远程提醒, 错误信息]\nline:%@\n-----\n%@\n\n", __func__, @(__LINE__), error);
+}
+
